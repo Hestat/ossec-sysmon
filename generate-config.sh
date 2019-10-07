a2fb8c80507263bd4285978c9cbeb4c26469735a
brian <@laskow26>
2019-10-06 21:07:06 -0400

    added new persistence rule and breaking out different powershell execution

--- generate-config.sh
+++ generate-config.sh
@@ -14,6 +14,7 @@ R805v9=$(pwd)/0805-v9-sysmon-modular_rules.xml
 R806=$(pwd)/0806-priv_esc_rules.xml
 R807=$(pwd)/0807-persistence_rules.xml
 R808=$(pwd)/0808-defense_evasion_rules.xml
+R809=$(pwd)/0809-execution_rules.xml
 RULESFILE=$(pwd)/local_rules.xml
 rm $RULESFILE
 
@@ -29,6 +30,7 @@ echo -e "Adding: \n$R804\n"; cat $R804 >> $RULESFILE
 echo -e "Adding: \n$R806\n"; cat $R806 >> $RULESFILE
 echo -e "Adding: \n$R807\n"; cat $R807 >> $RULESFILE
 echo -e "Adding: \n$R808\n"; cat $R808 >> $RULESFILE
+echo -e "Adding: \n$R809\n"; cat $R809 >> $RULESFILE
 
 echo -e "You can find your configuration file here:"
 echo -e "$RULESFILE"
