223189c5f5f72ff44ad8cc7b188c92c9fc8151a3
brian <@laskow26>
2019-10-15 19:34:07 -0400

    zipping log data

--- response-scripts/dfir.ps1
+++ response-scripts/dfir.ps1
@@ -1,3 +1,10 @@
+#######
+#Script to collect important logs and forensic triage information from a compromised system
+#
+#
+#
+
+$location="C:\Program Files (x86)\ossec-agent\logs\"
 $trace01="C:\Program Files (x86)\ossec-agent\logs\trace001$(get-date -f yyyy-MM-dd-hh-mm).etl"
 $pcap01="C:\Program Files (x86)\ossec-agent\logs\trace$(get-date -f yyyy-MM-dd-hh-mm).cap"
 $log01="C:\Program Files (x86)\ossec-agent\logs\summary$(get-date -f yyyy-MM-dd-hh-mm).log"
@@ -24,3 +31,5 @@ $s | Start-PefTraceSession
 
 wevtutil.exe epl Microsoft-Windows-Sysmon/Operational $log02
 wevtutil.exe epl "Microsoft-Windows-Windows Defender/Operational" $log03
+
+Compress-Archive -Path $trace01 $pcap01 $log01 $log02 $log03 -CompressionLevel NoCompression -DestinationPath $location\$env:USERNAME-$(get-date -f yyyy-MM-dd).zip
