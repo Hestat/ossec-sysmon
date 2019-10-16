5e9b2ab775cbacb943db4d4ffaac34d683a74704
brian <@laskow26>
2019-10-15 20:06:08 -0400

    aws stuff

--- response-scripts/dfir.ps1
+++ response-scripts/dfir.ps1
@@ -3,7 +3,7 @@
 #
 #
 #
-
+#Set paths and log variables
 $location="C:\Program Files (x86)\ossec-agent\logs\"
 $trace01="C:\Program Files (x86)\ossec-agent\logs\trace001$(get-date -f yyyy-MM-dd-hh-mm).etl"
 $pcap01="C:\Program Files (x86)\ossec-agent\logs\trace$(get-date -f yyyy-MM-dd-hh-mm).cap"
@@ -13,23 +13,59 @@ $log03="C:\Program Files (x86)\ossec-agent\logs\defender$(get-date -f yyyy-MM-dd
 $startdate="get-date"
 $comp="hostname"
 
-
+#####
+#gather basic data on the system
 quser.exe > $log01
 gdr -PSProvider 'Filesystem' >> $log01
 ps >> $log01
 
-
+#####
+#Collect 5 minutes of network activity (will be in microsoft event trace format, use the microsoft message analyzer to read)
+#Or use the commented out section below to move to pcap
 netsh trace start persistent=yes capture=yes traceFile=$trace01
-
+#change sleep if you want to gather data for a longer or shorter time frame
 sleep 300
 
 netsh trace stop
 
-$s = New-PefTraceSession -Path $pcap01 -SaveOnStop
-$s | Add-PefMessageProvider -Provider $trace01
-$s | Start-PefTraceSession
+#need microsoft message analyzer for this
+#$s = New-PefTraceSession -Path $pcap01 -SaveOnStop
+#$s | Add-PefMessageProvider -Provider $trace01
+#$s | Start-PefTraceSession
 
+#####
+#Dump pertinent logs
 wevtutil.exe epl Microsoft-Windows-Sysmon/Operational $log02
 wevtutil.exe epl "Microsoft-Windows-Windows Defender/Operational" $log03
 
-Compress-Archive -Path $trace01 $pcap01 $log01 $log02 $log03 -CompressionLevel NoCompression -DestinationPath $location\$env:USERNAME-$(get-date -f yyyy-MM-dd).zip
+####
+#Zip up our collected data
+Compress-Archive -Path $trace01, $log01, $log02, $log03, -CompressionLevel Optimal -DestinationPath $location\$env:USERNAME-$(get-date -f yyyy-MM-dd-hh-mm).zip
+
+#####
+#Clear logs
+#Clear-EventLog Microsoft-Windows-Sysmon/Operational
+
+
+$bucket = 'blazescan-signatures'
+$source = $location
+$region = 'us-west-2'
+
+Initialize-AWSDefaultConfiguration -Region $region
+ 
+Set-Location $source
+$files = Get-ChildItem '*.bak' | Select-Object -Property Name
+try {
+   if(Test-S3Bucket -BucketName $bucket) {
+      foreach($file in $files) {
+         if(!(Get-S3Object -BucketName $bucket -Key $file.Name)) { ## verify if exist
+            Write-Host "Copying file : $file "
+            Write-S3Object -BucketName $bucket -File $file.Name -Key $file.Name -CannedACLName private
+         } 
+      }
+   } Else {
+      Write-Host "The bucket $bucket does not exist."
+   }
+} catch {
+   Write-Host "Error uploading file $file"
+}
