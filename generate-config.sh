c0cb888b7576ca26403e88b87a1b535e19cb8501
brian <@laskow26>
2020-03-28 18:31:38 -0400

    rule merge 1

--- generate-config.sh
+++ generate-config.sh
@@ -15,6 +15,7 @@ R806=$(pwd)/0806-priv_esc_rules.xml
 R807=$(pwd)/0807-persistence_rules.xml
 R808=$(pwd)/0808-defense_evasion_rules.xml
 R809=$(pwd)/0809-execution_rules.xml
+R811=$(pwd)/0811-log_collection_rules.xml
 RULESFILE=$(pwd)/local_rules.xml
 rm $RULESFILE
 
@@ -31,6 +32,7 @@ echo -e "Adding: \n$R806\n"; cat $R806 >> $RULESFILE
 echo -e "Adding: \n$R807\n"; cat $R807 >> $RULESFILE
 echo -e "Adding: \n$R808\n"; cat $R808 >> $RULESFILE
 echo -e "Adding: \n$R809\n"; cat $R809 >> $RULESFILE
+echo -e "Adding: \n$R811\n"; cat $R811 >> $RULESFILE
 
 echo -e "You can find your configuration file here:"
 echo -e "$RULESFILE"
